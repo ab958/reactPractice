f858839b4399064fb9b33b1913d3d0f79ea07a32
Abdul Wahab <abwahab958@gmail.com>
2023-01-31 22:30:36 +0500

    modified card

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -2,39 +2,42 @@ import * as React from 'react';
 
 export default function BasicCard() {
     return (
-        // <Card sx={{ minWidth: 275 }}>
-        //   <CardContent>
-        //     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        //       Word of the Day
-        //     </Typography>
-        //     <Typography variant="h5" component="div">
-        //       be{bull}nev{bull}o{bull}lent
-        //     </Typography>
-        //     <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        //       adjective
-        //     </Typography>
-        //     <Typography variant="body2">
-        //       well meaning and kindly.
-        //       <br />
-        //       {'"a benevolent smile"'}
-        //     </Typography>
-        //   </CardContent>
-        //   <CardActions>
-        //     <Button size="small">Learn More</Button>
-        //   </CardActions>
-        // </Card>
-        <div className="card-category-1">
-            <div className="basic-card basic-card-dark">
-                <div className="card-content">
-                    <span className="card-title">Card Title</span>
-                    <p className="card-text">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                </div>
-                <div className="card-link">
-                    <a href="#" title="Read Full"><span>Read Full</span></a>
+        <>
+        <h2 style={{"margin": "30px"}}>Post Data </h2>
+            <div>
+                <form method="post" action="/" id="form" className="validate">
+                    <div className="form-field">
+                        <label>name</label>
+                        <input type="text" name="full-name" id="full-name" placeholder="Joe Bloggs" required />
+                    </div>
+                    <div className="form-field">
+                        <label>age</label>
+                        <input type="email" name="email-input" id="email-input" placeholder="66" required />
+                    </div>
+                    <div className="form-field">
+                        <label>colour</label>
+                        <input type="text" name="password-input" id="password-input" placeholder="blue" required />
+                    </div>
+                    <div className="form-field">
+                        <label ></label>
+                        <input type="submit" value="Post it" />
+                    </div>
+                </form>
+            </div>
+            <h2 style={{"margin": "30px"}}>Data From APi  </h2>
+            <div className="card-category-1">
+                <div className="basic-card basic-card-dark">
+                    <div className="card-content">
+                        <span className="card-title">Name is</span>
+                        <p className="card-text">
+                            Age is 
+                        </p>
+                        <p className="card-text">
+                            Colour is
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
